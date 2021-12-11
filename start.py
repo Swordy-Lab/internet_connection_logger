a74d79875509c3610c730ad0fdc2817386dd0f3a
Rene Schwertfeger <rene.schwertfeger1998@gmail.com>
2021-12-11 22:19:02 +0100

    update logs

--- start.py
+++ start.py
@@ -2,20 +2,23 @@ import rs_privat_module
 import datetime
 from time import sleep
 
+config = rs_privat_module.import_json("config.json")
+
 last = 0
 
 while(True):
-    status = rs_privat_module.fastping("google.de")
+    status = rs_privat_module.fastping(config["url"])
     
     if status:
-        print("Connected")
-        if last == 1:
-            rs_privat_module.Log_to_file("Connection restored", 0, "Logs", (str(datetime.date.today()) + ".log"))
+        if last == 0:
+            rs_privat_module.Log_to_file("Connected", 0, "Logs", (str(datetime.date.today()) + ".log"), True, False)
+        elif last == 1:
+            rs_privat_module.Log_to_file("Connection restored", 1, "Logs", (str(datetime.date.today()) + ".log"), True, True)
             last = 0
 
     else:
         if last == 0:
-            rs_privat_module.Log_to_file("Connection lost", 1, "Logs", (str(datetime.date.today()) + ".log"))
+            rs_privat_module.Log_to_file("Connection lost", 2, "Logs", (str(datetime.date.today()) + ".log"), True, True)
             last = 1
 
     sleep(5)
