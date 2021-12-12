fe1c7791858fc19ea584bc4869e4e47f3272da99
Rene Schwertfeger <rene.schwertfeger1998@gmail.com>
2021-12-12 11:34:27 +0100

    finalised

--- start.py
+++ start.py
@@ -4,30 +4,34 @@ from time import sleep, time
 import os
 config = rs_privat_module.import_json("config.json")
 
-if not os.path.exists("Logs"):
-    os.makedirs("Logs")
 
 last = 0
-downtime = 0 
+downtime = 0
 
 while(True):
+    today = str(datetime.date.today())
+    year,month,date = today.split("-")
+    rs_privat_module.check_and_create_folder("Logs" + "/" + year + "-" + month)
     status = rs_privat_module.fastping(config["url"])
-    
+
+    rs_privat_module.check_and_create_folder("Logs")
+    rs_privat_module.check_and_create_folder("Logs/")
+
     if status:
         if last == 0:
-            rs_privat_module.Log_to_file("Connected", 0, "Logs", (str(datetime.date.today()) + ".log"), True, True)
+            rs_privat_module.Log_to_file("Connected", 0, ("Logs" + "/" + year + "-" + month), (today + ".log"), True, True)
             last = 2
         elif last == 1:
             stop = time()
             downtime = stop - start
-            rs_privat_module.Log_to_file(("Connection restored" + " | Downtime " + str(int(downtime))), 1, "Logs", (str(datetime.date.today()) + ".log"), True, True)
+            rs_privat_module.Log_to_file(("Connection restored" + " | Downtime " + str(int(downtime))), 1, ("Logs" + "/" + year + "-" + month), (today + ".log"), True, True)
             last = 0
             if(os.path.exists("Logs/data.json")):
                 new_downtime = int(downtime) + int((rs_privat_module.import_json("Logs/data.json"))["Downtime"])
                 date = (rs_privat_module.import_json("Logs/data.json"))["started_at"]
             else:
                 new_downtime = int(downtime)
-                date = str(datetime.date.today())
+                date = today
             export = {"Downtime" : str(new_downtime), "started_at" : date}
             rs_privat_module.export_json("Logs/data.json", export, False)
 
@@ -35,11 +39,9 @@ while(True):
         if last == 0 or last == 2:
             if rs_privat_module.fastping("192.168.0.1"):
                 start = time()
-                rs_privat_module.Log_to_file("Connection lost", 2, "Logs", (str(datetime.date.today()) + ".log"), True, True)
+                rs_privat_module.Log_to_file("Connection lost", 2, ("Logs" + "/" + year + "-" + month), (today + ".log"), True, True)
                 last = 1
             else:
-                start = time()
-                rs_privat_module.Log_to_file("Connection lost. No connection to gateway.", 1, "Logs", (str(datetime.date.today()) + ".log"), True, True)
-                last = 1
+                rs_privat_module.Log_to_file("Connection lost. No connection to gateway.", 1, ("Logs" + "/" + year + "-" + month), (today + ".log"), True, True)
 
     sleep(5)
